9c89c9fcfdce52090ef3f34b470f49a020c08423
Your Name <you@example.com>
2015-05-20 17:34:10 +0200

    Just created and updated Services

--- src/test/java/scomanagementsystem/domain/ServicesTest.java
+++ src/test/java/scomanagementsystem/domain/ServicesTest.java
@@ -25,11 +25,37 @@ public class ServicesTest {
         Map<String,String> values = new HashMap<String,String>();
 
         values.put("date","15 May 2015");
-        values.put("vanue", "2.70 Science Building");
+        values.put("venue", "2.70 Science Building");
 
         Services course = ServicesFactory
                 .createService(100, "Academic Service", values);
 
         Assert.assertEquals(100, course.getServiceNo());
+    }
+
+    @Test
+    public void testUpadteService() throws Exception {
+        Map<String,String> values = new HashMap<String,String>();
+
+        values.put("date","15 May 2015");
+        values.put("venue", "2.70 Science Building");
+
+        Services service =ServicesFactory
+                .createService(100, "Academic Service", values);
+        //updating
+        Map<String,String> updatevalues = new HashMap<String,String>();
+
+        updatevalues.put("date","22 May 2015");
+        updatevalues.put("venue", "2.71 Science Building");
+        Services newservice = new Services
+                .Builder(service.getServiceNo())
+                .serviceName("Academic Service")
+                .copy(service)
+                .date(updatevalues.get("date"))
+                .venue(updatevalues.get("venue"))
+                .build();
+
+        Assert.assertEquals("22 May 2015",newservice.getDate());
+        Assert.assertEquals("2.71 Science Building",newservice.getServiceName());
     }
 }
